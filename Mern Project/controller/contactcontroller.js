079c40873a403ef4fb31231d80aa1f9b5df97db3
seervi-mandira <mandirao186@gmail.com>
2025-07-31 20:56:38 +0530

    Mern project

--- Mern Project/controller/contactcontroller.js
+++ Mern Project/controller/contactcontroller.js
@@ -1,12 +1,34 @@
 
-const contacts = require('../model/contactschema')
+// const contacts = require('../model/contactschema')
+// const contactform = async (req, res) => {
+// try {
+//        const response = req.body;
+//        await contacts.create(response)
+//        res.status(200).json({mes:'sucressfull done '})   
+// } catch (error) {
+//             res.status(400).json({mes:'unable to send '}) 
+// }
+// }
+//  module.exports = contactform;
+const Contact = require('../model/contactschema');
+
 const contactform = async (req, res) => {
-try {
-       const response = req.body
-       await contacts.create(response)
-       res.status(200).json({mes:'sucressfull done '})   
-} catch (error) {
-            res.status(400).json({mes:'unable to send '}) 
-}
-}
- module.exports = contactform;
+  try {
+    const { username, email, message } = req.body;
+
+    // Validation
+    if (!username || !email || !message) {
+      return res.status(400).json({ msg: "All fields are required" });
+    }
+
+    // Save to DB
+    const newContact = await Contact.create({ username, email, message });
+
+    res.status(201).json({ msg: "Contact form submitted successfully", data: newContact });
+  } catch (error) {
+    console.error("Contact form error:", error.message);
+    res.status(500).json({ msg: "Something went wrong", error: error.message });
+  }
+};
+
+module.exports = contactform;
--- Mern Project/router/contactroute.js
+++ Mern Project/router/contactroute.js
@@ -4,8 +4,9 @@ const express = require('express');
 const router = express.Router();
 
 const { contactdatas, DeleteContactdata } = require('../controller/admincontact');
+const contactform = require('../controller/contactcontroller');
 
 router.route('/contact').get(contactdatas);
 router.route('/delete/:id').delete(DeleteContactdata);
-
+router.route('/contacts').post(contactform)
 module.exports = router;
